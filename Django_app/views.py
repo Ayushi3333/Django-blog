c48145c679cdaa6a777d46427fa1a4eb16da89a3
ayushi gupta <ayushi3101gupta@gmail.com>
2021-04-16 15:26:47 +0200

    Basic template and routing

---
+++ Django_app/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):
+	  my_title = "Hello hey there......." 
+	  return render(request, "hello_world.html", {"title": my_title})
+
+def about_page(request):
+		return render(request, "hello_world.html", {"title": "About"})
+
+def contact_page(request):
+		return render(request, "hello_world.html", {"title": "Contact us"})

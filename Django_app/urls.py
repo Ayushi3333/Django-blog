68ac782202b333f5106c7493fd246db8278ced6f
ayushi gupta <ayushi3101gupta@gmail.com>
2021-04-25 18:45:00 +0200

    add CRUD in views and template

--- Django_app/urls.py
+++ Django_app/urls.py
@@ -21,12 +21,20 @@ from .views import (
     contact_page
 )
 from post.views import (
-    blog_post_detail_page
+    blog_post_detail_view,
+    blog_post_list_view,
+    blog_post_create_view,
+    blog_post_update_view,
+    blog_post_delete_view
 )
 urlpatterns = [
     path('', home_page),
+    path('blog-new/', blog_post_create_view),
+    path('blogs/', blog_post_list_view),
+    path('blog/<str:slug>/edit/', blog_post_update_view),
+    path('blog/<str:slug>/delete/', blog_post_delete_view),
     # path('blog/<int:post_id>/', blog_post_detail_page), # dynamic routing
-    path('blog/<str:slug>/', blog_post_detail_page), # routing with slug
+    path('blog/<str:slug>/', blog_post_detail_view), # routing with slug
     path('about/', about_page),
     path('contact/', contact_page),
     path('admin/', admin.site.urls),
--- post/views.py
+++ post/views.py
@@ -1,23 +1,42 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
-
-
-# Create your views here.
 from .models import BlogPost # relative import
 
-# GET -> 1 object
-# filter -> [] objects
+def blog_post_list_view(request):
+    queryset = BlogPost.objects.all()
+    template_name = 'blog_post_list.html'
+    context = { 'object_list': queryset }
+    return render(request, template_name, context)
 
-def blog_post_detail_page(request, slug):
+def blog_post_create_view(request):
+    template_name = 'blog_post_create.html'
+    context = { 'form': None }
+    return render(request, template_name, context)
+
+def blog_post_detail_view(request, slug):
     # obj = get_object_or_404(BlogPost, id=post_id) # with error handling and lookup by id
     # obj = BlogPost.objects.get(id=post_id) # without error handling
-    # obj = get_object_or_404(BlogPost, slug=slug) # lookup by slug field
-    
-    queryset = BlogPost.objects.filter(slug=slug) # returns a Query Set of all objects meeting that condition
-    if queryset.count() == 0:
-        raise Http404
-    obj = queryset.first()
+    obj = get_object_or_404(BlogPost, slug=slug) # lookup by slug field
+    # print("HELLO DJANGO", request.method, request.path, request.user)
+
+    # queryset = BlogPost.objects.filter(slug=slug) # returns a Query Set of all objects meeting that condition
+    # if queryset.count() == 0:
+    #     raise Http404
+    # obj = queryset.first()
 
     template_name = 'blog_post_detail.html'
     context = {"object": obj}
     return render(request, template_name, context)
+
+def blog_post_update_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug) # lookup by slug field
+    template_name = 'blog_post_update.html'
+    context = {"object": obj, 'form': None}
+    return render(request, template_name, context)
+
+def blog_post_delete_view(request):
+    obj = get_object_or_404(BlogPost, slug=slug) # lookup by slug field
+    template_name = 'blog_post_delete.html'
+    context = {"object": obj}
+    return render(request, template_name, context)
+
